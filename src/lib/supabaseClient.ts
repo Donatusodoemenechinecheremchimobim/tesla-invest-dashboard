f7b834e12b24b766134a7d810ef350e709c572fc
Donatus-Odoemene Chinecherem Chimobim <chibundusadiq@gmail.com>
2026-02-15 15:01:39 +0100

    Fix formatting in supabaseClient.ts

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,5 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
+// Project: wazjqsbrafnxpzrrekhu
 const supabaseUrl = "https://wazjqsbrafnxpzrrekhu.supabase.co";
 const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Indhempxc2JyYWZueHB6cnJla2h1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzExNTQ2MDgsImV4cCI6MjA4NjczMDYwOH0.VwG73_vS_qDQjTNKUHOUBukR2fdKlYqz7jh8YdXsZd0";
 
